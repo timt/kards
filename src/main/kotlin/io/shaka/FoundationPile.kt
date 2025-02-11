3c941e22dd7dfd07d32bad7fd65e9d040710abee
timt <ttennant@gmail.com>
2025-02-11 21:26:13 +0000

    Extract extension functions

--- src/main/kotlin/io/shaka/FoundationPile.kt
+++ src/main/kotlin/io/shaka/FoundationPile.kt
@@ -1,11 +1,11 @@
 package io.shaka
 
-data class FoundationPile(val suit: Suit, val cards: List<Card> = emptyList()) {
-    fun addCard(card: Card): FoundationPile {
-        if (card.suit != suit) throw IllegalArgumentException("Card $card is not the correct suit for this foundation pile: $suit")
-        if (card.rank != Rank.ACE && cards.isNotEmpty() && card.rank.defaultOrder != (cards.last().rank.defaultOrder + 1)) {
-            throw IllegalArgumentException("Card $card is not the next rank for this foundation pile: $cards")
-        }
-        return this.copy(cards = cards + card)
+data class FoundationPile(val suit: Suit, val cards: List<Card> = emptyList())
+
+fun FoundationPile.addCard(card: Card): FoundationPile {
+    if (card.suit != suit) throw IllegalArgumentException("Card $card is not the correct suit for this foundation pile: $suit")
+    if (card.rank != Rank.ACE && cards.isNotEmpty() && card.rank.defaultOrder != (cards.last().rank.defaultOrder + 1)) {
+        throw IllegalArgumentException("Card $card is not the next rank for this foundation pile: $cards")
     }
+    return this.copy(cards = cards + card)
 }
--- src/main/kotlin/io/shaka/patience/shenzhen/TableauPile.kt
+++ src/main/kotlin/io/shaka/patience/shenzhen/TableauPile.kt
@@ -2,14 +2,14 @@ package io.shaka.patience.shenzhen
 
 import io.shaka.Card
 
-data class TableauPile(val cards: List<Card> = emptyList()) {
-    fun addCard(card: Card): TableauPile{
-        if (cards.isNotEmpty() && card.suit.color == cards.last().suit.color) {
-            throw IllegalArgumentException("Card ($card) colors must be alternated")
-        }
-        if (cards.isNotEmpty() && card.rank.defaultOrder != cards.last().rank.defaultOrder - 1) {
-            throw IllegalArgumentException("Card ($card) must be one rank lower than the last card in the pile")
-        }
-        return this.copy(cards = cards + card)
+data class TableauPile(val cards: List<Card> = emptyList())
+
+fun TableauPile.addCard(card: Card): TableauPile{
+    if (cards.isNotEmpty() && card.suit.color == cards.last().suit.color) {
+        throw IllegalArgumentException("Card ($card) colors must be alternated")
     }
+    if (cards.isNotEmpty() && card.rank.defaultOrder != cards.last().rank.defaultOrder - 1) {
+        throw IllegalArgumentException("Card ($card) must be one rank lower than the last card in the pile")
+    }
+    return this.copy(cards = cards + card)
 }
